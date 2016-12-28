d4c20faf864abb782f6397385bdb92347c20a024
Joshua W <jwu910@yahoo.com>
2016-12-28 09:59:26 -0800

    Added function to draw card

--- BlackJack.py
+++ BlackJack.py
@@ -29,17 +29,27 @@ Player_Names = []
 for player in range(Player_Num):
 	Player_Names.append(raw_input("Enter player names = "))
 
-print Player_Names
-Card_Suit = r.randint(1,4) # Generate card suit
-Card_Num = r.randint(1,13) # Generate card number
+print Player_Names # Print just for testing
 
-print Card_Suit, Card_Num
+def draw_card():
+	Suit = {1:'H',2:'D',3:'S',4:'C'} 
+	Card_Suit = r.randint(1,4) # Generate card suit
+	Card_Num = r.randint(1,13) # Generate card number
+	Card = str(Suit[Card_Suit]) + str(Card_Num)
+	return(Card)
 
 # This is the deck, use index value for card number, Boolean for status of card in deck.
+
 Deck = [[True,True,True,True,True,True,True,True,True,True,True,True,True],
 		[True,True,True,True,True,True,True,True,True,True,True,True,True],
 		[True,True,True,True,True,True,True,True,True,True,True,True,True],
 		[True,True,True,True,True,True,True,True,True,True,True,True,True]]
 		
-print Deck[Card_Suit-1][Card_Num-1]
-Suit = ['H', 'D', 'S', 'C'] 
+
+#print Deck[Card_Suit-1][Card_Num-1] # Print just for testing
+
+
+
+#print Card_Suit, Card_Num # Print just for testing
+print draw_card()
+# Hand cards out for players
