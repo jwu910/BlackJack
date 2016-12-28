f90ead822cf002f7602a2475534ebc9e5d6b9a8d
jwu910 <jwu910@yahoo.com>
2016-12-28 09:00:14 -0800

    added try/except to catch data errors on user input

--- BlackJack.py
+++ BlackJack.py
@@ -5,8 +5,26 @@ import random as r #use for generating card values
 
 
 # User input for number of players
-Player_Num = input("Number of Players = ") #Max 6 Players
+try: # Need to learn how to add a loop on exception errors to request num 
+	 # of players again instead of quit
+	Player_Num = input("Number of Players = ") #Max 6 Players
+except NameError:
+	print "Invalid data type, must be number 1-6."
+	quit()
+except SyntaxError:
+	print "Invalid data! Must enter number of players!"
+	quit()
 
+# Check if number of players is valid	
+if Player_Num < 1:
+	print "Need at least 1 player!"
+	quit()
+elif Player_Num > 6:
+	print "Too many players! Max 6 players."
+	quit()
+
+	
+# Assign player names
 Player_Names = []
 for player in range(Player_Num):
 	Player_Names.append(raw_input("Enter player names = "))
