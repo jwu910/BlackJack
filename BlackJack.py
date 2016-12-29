7f5fc9b202d1b80edf401903cc424ef04be4bcd4
Joshua W <jwu910@yahoo.com>
2016-12-28 16:24:51 -0800

    added a 'current player' screen and screen clear as game progresses

--- BlackJack.py
+++ BlackJack.py
@@ -1,8 +1,8 @@
 # Black Jack
 # Create a Black Jack game in Python 2.7
 
-import random as r #use for generating card values
-
+import random as r # use for generating card values
+import os # to clear screen
 # Create a class of Player with Name and Hand
 class Player(object):
 
@@ -12,6 +12,11 @@ class Player(object):
 		
 	def add_card(self, card):
 		self.hand.append(card)
+		
+	def status(self, status):
+		self.status = status
+	# def fold(self, p):
+		# self.hand.pop(p)
 		
 # User input for number of players
 try: # Need to learn how to add a loop on exception errors to request num 
@@ -44,6 +49,9 @@ for i in range(len(Players)):
 	Players[i] = Player(Players[i])
 	print Players[i].name
 	
+for i in range(len(Players)):
+	Players[i].status = "Playing"
+	
 
 	
 # This is the deck, use index value for card number, Boolean for status of card in deck.	
@@ -75,32 +83,64 @@ def draw_card(): # this function should draw card and return only card that was 
 # Hand cards out for players
 Drawn = 0
 cont = False
-
+def print_current():
+	os.system('cls')
+	print "Current Players = "
+	for i in range(len(Players)):
+		print Players[i].name, Players[i].status
+		print Players[i].hand
+	print "---------------------------------------------"
+	print "Total Cards on Table = ", Drawn
 
 while Drawn < 52:
-	print "Total Cards on Table = ", Drawn
+	print_current()
 	#for i in #######################
-	
-	cont = raw_input("Continue? Y/N = ")
-	if cont == "Y":
-		if Drawn >= 52:
-				for i in range(0,len(Players)):
-					p = Players[i]
-					print p.name, p.hand, "Hand total = ", sum(p.hand)
-					quit()
-		elif 52 - Drawn < Player_Num+1:
-			print "Not enough cards."
-			print 52-Drawn, " Cards left"
-			quit()
-		else:
-			for i in range(0,Player_Num+1):
+	for i in range(0,len(Players)):
+		try:
+			if sum(Players[i].hand) > 21:
 				p = Players[i]
-				Card = draw_card()
-				cardface = str(Suit[Card[0]]) + str(Card[1])
-				p.add_card(Card[2]) # Need to add card as card value
-				Drawn += 1 # Count number of cards drawn
-				print "Player = ", p.name, "Card Drawn = ", cardface
-				print "Hand total = ", sum(p.hand)# Print cards dealt this round
+				print p.name, "'s hand is over 21! Bust!!"
+				print p.name, " is out of the game!"
+				# Players.pop(i)
+				p.status = "Folded!"
+				print i
+		except IndexError:
+			print i
+			print Players
+			pass
+	if Drawn >= 52:
+			for i in range(0,len(Players)):
+				print p.name, p.hand, "Hand total = ", sum(Players[i].hand)
+				quit()
+	elif 52 - Drawn < len(Players):
+		print "Not enough cards."
+		print 52-Drawn, " Cards left"
+		quit()
 	else:
-		print "Paused, enter 'Y' to continue."
+		
+		
+		for i in range(0,len(Players)):
+			p = Players[i]
+			if p.status == "Playing":
+			# Add sum(p.hand) and see if player would like to hit or stay
+				print "Current Player: ", p.name
+				print "Hand total: ", sum(p.hand)
+				if p.name == "Dealer":
+					hit = "H"
+				else:
+					hit = raw_input("Hit or Stay? H/S = ")
+				if hit == "H":
+					Card = draw_card()
+					cardface = str(Suit[Card[0]]) + str(Card[1])
+					p.add_card(Card[2]) # Need to add card as card value
+					Drawn += 1 # Count number of cards drawn
+					print_current()
+					print "---------------------------------------------"
+					print "Player: ", p.name, "Card Drawn: ", cardface
+					print "Hand total: ", sum(p.hand)# Print cards dealt this round
+					print "---------------------------------------------"
+				elif hit == "S":
+					continue
+			elif p.status == "Folded!":
+				next
 
