2e67cc31c0b7efd7469309462cea808569a5a7cd
Joshua <jwu910@yahoo.com>
2017-01-02 01:43:41 -0800

    got working player turns and winning conditions after dealer stands/busts.

--- BlackJack.py
+++ BlackJack.py
@@ -2,7 +2,7 @@
 # Create a Black Jack game in Python 2.7
 
 import random as r # use for generating card values
-import os # to clear screen
+import os , time # OS to clear screen, time to pause
 
 # Create a class of Player with Name and Hand
 class Player(object):
@@ -58,6 +58,9 @@ for i in range(len(Players)):
 	Players[i].status = "Playing"
 	
 # This is the deck, use index value for card number, Boolean for status of card in deck.
+
+Drawn = 0
+
 Suit = {1:'H',2:'D',3:'S',4:'C'} 
 Deck = [[True,True,True,True,True,True,True,True,True,True,True,True,True],
 		[True,True,True,True,True,True,True,True,True,True,True,True,True],
@@ -98,10 +101,18 @@ def print_current():
 		print "Total Cards on Table = ", Drawn
 		print "---------------------------------------------"
 
-
-Drawn = 0
+def hit_stand():
+	while True:
+		hit = raw_input("Hit or Stand? H/S = ")
+		if hit.upper() == "H" or hit.upper() == "S":
+			return hit.upper()
+			break
+		else:
+			print "Invalid answer."
+	return hit.upper()
+		
 valid_answers = ['H','S']
-
+hit = ""
 
 # Draw initial 2 cards for players
 for player in Players:
@@ -110,6 +121,7 @@ for player in Players:
 		if p.name == "Dealer" and len(p.hand) > 0:
 			Card = draw_card()
 			cardface = "***"
+			hidden = str(Suit[Card[0]]) + str(Card[1])
 			p.add_card(Card[2],cardface)
 		else:
 			Card = draw_card()
@@ -122,32 +134,73 @@ for player in Players:
 		
 # -----------------------------Deal---------------------------------------
 print_current()
+#
+#	print p.name, p.status
 for i in range(0,len(Players)):
 	p = Players[i]
-	while p.name != "Dealer" and p.status == "Playing":
-		print_current()
-		print 'Current player is =',p.name
-		while True:
-			hit = raw_input("Hit or Stay? H/S = ")
-			if hit.upper == answer.upper() in valid_answers:
-				if hit.upper == "H":
-					Card = draw_card()
-					cardface = str(Suit[Card[0]]) + str(Card[1])
-					p.add_card(Card[2],cardface)
-					Drawn += 1
-					print_current()
-					if sum(p.hand) > 21:
-						p.status = "busted!"
-						print_current()
-						continue
-						break
-					print_current()
-				elif hit.upper == "S":
-					p.status = "standing."
-					print_current()
-					break
-				else:
-					print "this is where we're stuck"
-	if sum(p.hand) > 21:
-		p.status = "busted!"
+	if p.name != "Dealer": # Perform actions for players
+		while sum(p.hand) <22:
+			hit = hit_stand()
+			if hit == "H":
+				Card = draw_card()
+				cardface = str(Suit[Card[0]]) + str(Card[1])
+				p.add_card(Card[2],cardface)
+				Drawn += 1
+				print_current()
+			elif hit == "S":
+				p.status = "standing."
+				print_current()
+				break
+
+		if sum(p.hand) > 21:
+			p.status = "busted!"
+			print_current()
+			break
 	
+	elif p.name == "Dealer": # Perform Dealer actions
+		while sum(p.hand) < 17:
+			hit = "H"
+			time.sleep(1)
+			if hit == "H":
+				Card = draw_card()
+				cardface = str(Suit[Card[0]]) + str(Card[1])
+				p.add_card(Card[2],cardface)
+				p.card_faces[1] = hidden
+				Drawn += 1
+				print_current()
+			elif hit == "S":
+				p.status = "standing."
+				p.card_faces[1] = hidden
+				print_current()
+				break
+				
+		if sum(p.hand) >= 17:
+			p.status = "standing."
+			p.card_faces[1] = hidden
+			print_current()
+		if sum(p.hand) > 21:
+			p.status = "busted!"
+			print_current()
+			break
+		
+################################################# End player turns ########################################################
+# Start results checking
+index = 0
+results = []
+
+for i in Players[::]:
+	p = Players[index]
+	if sum(p.hand) < 22:
+		results.append(sum(p.hand))
+	else:
+		results.append(0)
+	index += 1
+	
+
+index = 0
+for i in Players[::]:
+	p = Players[index]
+	if sum(p.hand) == max(results):
+		print p.name, "wins!"
+	index += 1
+		
